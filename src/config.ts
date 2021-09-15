5a7402891458ee1c5cad64d080b35a62c1964d07
zzboncak <zachary.zboncak@gmail.com>
2021-09-15 09:58:19 -0500

    added deployed server url

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,4 @@
-export const API_ENDPOINT = "http://localhost:8000/api";
+export const API_ENDPOINT =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:8000/api"
+    : "https://hidden-cove-25106.herokuapp.com/api";
