7d10489aede495e5a6485ad9e5cbc9348c0119df
zzboncak <zachary.zboncak@gmail.com>
2021-12-20 20:14:24 -0600

    edited display of people status

--- src/PeopleStatus.tsx
+++ src/PeopleStatus.tsx
@@ -13,7 +13,11 @@ export const PeopleStatus: React.FC<{ status: RSVP_Options }> = ({
     fetchAllInvitesAndPeople().then(([invites, people]) => {
       const peopleToDisplay = people.filter(
         (person) =>
-          person.rsvp === status || person.extra_confirmed === status
+          person.rsvp === status ||
+          person.extra_confirmed === status ||
+          (status === RSVP_Options.NO_RESPONSE &&
+            person.allowed_extra &&
+            !person.extra_confirmed)
       );
       const sortedPeople = peopleToDisplay.sort(
         (a, b) => a.family_id - b.family_id
@@ -29,13 +33,30 @@ export const PeopleStatus: React.FC<{ status: RSVP_Options }> = ({
           (invite) => invite.id === person.family_id
         )?.keyword;
         return (
-          <Link
-            to={`/edit-invite/${keyword}`}
-            key={person.id}
-            className="person-link"
-          >
-            {person.first_name} {person.last_name} {person.rsvp}
-          </Link>
+          <>
+            {status === person.rsvp && (
+              <Link
+                to={`/edit-invite/${keyword}`}
+                key={person.id}
+                className="person-link"
+              >
+                {person.first_name} {person.last_name} {person.rsvp}
+              </Link>
+            )}
+            {person.allowed_extra &&
+              status === RSVP_Options.NO_RESPONSE && (
+                <Link
+                  to={`/edit-invite/${keyword}`}
+                  key={`${person.id}-extra`}
+                  className="person-link"
+                >
+                  {person.first_name} {person.last_name}&apos;s extra:
+                  {` ${
+                    person.extra_confirmed ?? RSVP_Options.NO_RESPONSE
+                  }`}
+                </Link>
+              )}
+          </>
         );
       })}
     </section>
